5ca067b4c8d4d8c0f9337beb1e19f33dcc419b29
aziznal <aziznal.dev@gmail.com>
2021-06-02 22:17:58 +0300

    Organizes and annotates main file

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
+from typing import Callable, List, Literal, Tuple
 import pygame
-
-game_clock = pygame.time.Clock()
 
 from GameSettings import *
 
-
 from Events import handle_events, handle_player_start_game, handle_quit
-
 from Player import Player
-
 from Pipe import Pipe
-
-
 
 pygame.init()
 pygame.font.init()
 
+game_clock = pygame.time.Clock()
 
-def get_screen():
+
+def get_screen() -> pygame.Surface:
+    """
+    Returns screen object
+    """
     return pygame.display.set_mode((ScreenSettings.width, ScreenSettings.height))
 
 
-
-def get_text(text, size, color, position):
+# TODO: replace this with class for Text Objects to make manipulating them easier
+def get_text(
+    text: str,
+    size: int,
+    color: Colors,
+    position: Tuple[float, float]
+    ) -> Tuple[pygame.Surface, Callable[[pygame.Surface], None]]:
 
     font = pygame.font.SysFont("arial", size)
 
@@ -31,9 +35,10 @@ def get_text(text, size, color, position):
     return img, lambda screen: screen.blit(img, position)
 
 
-
-
-def wait_for_player_to_start_game(screen, draw_functions):
+def display_start_screen(
+    screen: pygame.Surface,
+    draw_functions: List[Callable[[pygame.Surface], None]]
+    ) -> None:
 
     start_text, start_text_draw = get_text("Press Space to Start", size=56, color=Colors.black, position=(ScreenSettings.width/2 - 300, ScreenSettings.height/2 - 200))
 
@@ -58,41 +63,38 @@ def wait_for_player_to_start_game(screen, draw_functions):
         game_clock.tick(60)
 
 
-def on_player_death(screen, draw_functions):
+def on_player_death(
+    screen: pygame.Surface,
+    draw_functions: List[Callable[[pygame.Surface], None]]
+    ) -> None:
     
     while True:
 
-
         handle_quit(pygame.event.get())
-
 
         screen.fill(Colors.screen_background)
 
         for func in draw_functions:
             func(screen)
 
-
         pygame.display.flip()
 
         game_clock.tick(60)
 
 
-def run_game(screen, draw_functions, update_funcs, player):
-
-    screen = get_screen()
-
-
-    wait_for_player_to_start_game(screen, draw_functions)
-
-
+def start_game_loop(
+    screen: pygame.Surface,
+    draw_functions: List[Callable[[pygame.Surface], None]],
+    update_functions: List[Callable[[], None]], 
+    player: Player
+    ) -> None:
     while True:
 
-        handle_events(on_player_jump=lambda: birb.jump())
+        handle_events(on_player_jump=lambda: player.jump())
 
         screen.fill(Colors.screen_background)
 
-
-        for func in update_funcs:
+        for func in update_functions:
             func()
 
         for func in draw_functions:
@@ -101,13 +103,25 @@ def run_game(screen, draw_functions, update_funcs, player):
         pygame.display.flip()
 
         game_clock.tick(60)
-        
 
+
+def run_game(
+    screen: pygame.Surface,
+    draw_functions: List[Callable[[pygame.Surface], None]],
+    update_functions: List[Callable[[], None]],
+    player: Player
+    ) -> None:
+
+    display_start_screen(screen, draw_functions)
+
+    start_game_loop(screen, draw_functions, update_functions, player)
+        
 
 if __name__ == '__main__':
 
     screen = get_screen()
 
+    # REFACTOR: change these into TextObject class after that's been created
     score, score_draw = get_text("Score", 30, Colors.black, (ScreenSettings.width / 2, 20))
     gameover_text, gameover_text_draw = get_text(
         "Game over!",
@@ -118,6 +132,8 @@ if __name__ == '__main__':
 
     birb = Player(on_death=lambda: on_player_death(screen, [gameover_text_draw]))
 
+    # TODO: create 'Pipes' class to make drawing and colliding with pipes syntatically cleaner
+    # REFACTOR: create these using Pipes class after that's been created
     top_pipe = Pipe(offset=0, player=birb)
     bottom_pipe = Pipe(offset=0, side="BOTTOM", top_pipe=top_pipe, player=birb)
 
@@ -127,13 +143,14 @@ if __name__ == '__main__':
     top_pipe2 = Pipe(offset=1000, player=birb)
     bottom_pipe2 = Pipe(offset=1000, side="BOTTOM", top_pipe=top_pipe2, player=birb)
 
-    # TODO: create 'Pipes' class to make drawing and colliding with pipes syntatically cleaner
 
     run_game(
 
-        screen,
+        screen=screen,
 
-        [
+        player=birb,
+
+        draw_functions=[
             birb.draw,
 
             top_pipe.draw,
@@ -149,7 +166,7 @@ if __name__ == '__main__':
 
         ],
 
-        [   
+        update_functions=[   
 
             birb.update,
 
@@ -162,9 +179,6 @@ if __name__ == '__main__':
             top_pipe2.update,
             bottom_pipe2.update,
 
+        ]
 
-        ],
-
-        birb
-
-        )
+    )
